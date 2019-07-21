1df6efecbe409dc924a9936f12fd890124285005
Dolores <dolores.macauley@gmail.com>
2019-07-21 17:36:19 -0400

    first commit: base functionality

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Selector from './App';
+
+
+ReactDOM.render(<Selector />, document.getElementById('root'));
